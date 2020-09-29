802ca28589635d9958657930c66c91100c9d41e2
liuzhe <zhe.liu@microsoft.com>
2020-09-29 09:02:53 +0800

    restructure files

---
+++ HardCode.cs
@@ -0,0 +1,10 @@
+namespace Nni {
+    class HardCode {
+        public static string NodePath = @"C:\_code\nni-lib\nni-manager\node.exe";
+        public static string NniManagerPath = @"C:\_code\nni-lib\nni-manager\dist";
+        public static string TrialDir = @"C:\_code\nni-lib";
+        public static string TrialCommand = @".\bin\Debug\netcoreapp3.1\nni-lib.exe --trial ";
+        public static string CsPipePath = "nni-pipe";
+        public static string NodePipePath = @"\\.\pipe\nni-pipe";
+    }
+}
